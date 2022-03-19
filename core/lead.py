aa984a8b617ac1e2cafe1b780eb953bb86099411
jdineshbabu <jdineshbabu@live.com>
2022-03-19 20:27:16 +0530

    Completed lead feature development.
    Handing over the Lead feature to QA team for testing purpose.

---
+++ core/lead.py
@@ -0,0 +1,17 @@
+##
+#Lead feature functionalities.
+##
+
+#Create Lead
+def create_lead():
+	print "created lead succesfully"
+
+
+#update lead
+def update_lead():
+	print "lead updated succesfully"
+
+#delete lead
+def delete_lead();
+	print	"lead deleted succesfully"
+
