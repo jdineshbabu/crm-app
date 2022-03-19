2fdd2f2ca3c24bda4ee5622b29b8afec4a8b1531
jdineshbabu <jdineshbabu@live.com>
2022-03-19 19:54:48 +0530

    Lead feature development is in progress
    Completed Lead Update functionality

---
+++ core/lead.py
@@ -0,0 +1,13 @@
+##
+#Lead feature functionalities.
+##
+
+#Create Lead
+def create_lead():
+	print "created lead succesfully"
+
+
+#update lead
+def update_lead():
+	print "lead updated succesfully"
+
