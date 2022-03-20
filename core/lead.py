a550a8d0de98c362895e2f9e2de7489052673cab
jdineshbabu <jdineshbabu@live.com>
2022-03-20 17:34:34 +0530

    Included delete lead function
    By Dev1

--- core/lead.py
+++ core/lead.py
@@ -6,12 +6,15 @@
 def create_lead():
 	print "created lead succesfully"
 
-
 #update lead
 def update_lead():
 	print "lead updated succesfully"
 
 #delete lead
 def delete_lead();
-	print	"lead deleted succesfully"
+	print	"lead removed succesfully"
+
+#View lead
+def view_lead();
+	print	"View the lead"
 
